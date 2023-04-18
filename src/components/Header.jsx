6f0f395a7b19d27992f8383c0eb6d50144e61af6
Sayan <93570827+sayankumarbag7@users.noreply.github.com>
2023-04-19 01:57:57 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const react = require("react");
+
+function Header() {
+  return (
+    <header>
+      <h1>Notes</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React.js Bootcamp</h1>
+      <p>I learnt things like React , Javascript and created a project</p>
+    </div>
+  );
+}
+export default Notes;
